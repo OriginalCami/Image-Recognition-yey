205a0352499912ce104c0922f620cda8d03d25fc
Camilla <91672326+OriginalCami@users.noreply.github.com>
2022-04-17 21:03:48 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -19,4 +19,24 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/d_7jRy78s/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/4kC5mcEXy/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_object").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
